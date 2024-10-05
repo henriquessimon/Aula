ff9729640d57b81bbc95beb644aed965ebfbf7c0
Henriques_Simon <78738745+henriquessimon@users.noreply.github.com>
2024-10-04 21:15:50 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pessoa {
+    
+    public static void main() {
+        Scanner ler = new Scanner(System.in)
+        int cpf = ler.nextInt;
+    }
+
+    public pessoa() {
+
+    }
+}
